2eb2293d22372c4b923244962be23c4abd665f76
randomuserJ <goliasdavid65@gmail.com>
2020-03-17 20:57:50 +0100

    minor changes in backend

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ class Resource(object):
                     'href': '/images/1eaf6ef1-7f2d-4ecc-a8d5-6e8adba7cc0e.png'
                 }
             ]
-        }
+        } 
 
         # Create a JSON representation of the resource
         resp.body = json.dumps(doc, ensure_ascii=False)
