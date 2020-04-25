c9fb7e77dfec60caae0b39b5d4a13c77da96f5db
Vanesa Andicsova <vanesaandicsova@gmail.com>
2020-04-25 09:19:08 +0100

    Middlewares and seeder

---
+++ backend/app/seeder.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from models import Question, Diagnose, Option
+from self_diagnostic_component import self_diagnostic_component
+
+postgres = f'postgresql://postgres:password@db:5432/rehabApp'
+engine = create_engine(postgres, echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+models = {}
+
+session = Session()
+
+for key, item in self_diagnostic_component.items():
+    if item['type'] == "question":
+        model = Question(text=item['text'])
+    else:
+        model = Diagnose(name=item['name'], text=item['text'])
+
+    session.add(model)
+    models[key] = model
+
+session.commit()
+
+for key, item in self_diagnostic_component.items():
+    if item['type'] == "question":
+        for option in item['options']:
+            ref = option['ref']
+            if self_diagnostic_component[ref]["type"] == "question":
+                model = Option(question_id=models[key].id, label=option['label'], question=models[ref])
+            else:
+                model = Option(question_id=models[key].id, label=option['label'], diagnose=models[ref])
+
+            session.add(model)
+
+session.commit()
+session.close()
