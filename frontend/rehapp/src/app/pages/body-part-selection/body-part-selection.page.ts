c9ffe76b7de573e56607f5bbbf1d9357bce7b071
randomuserJ <goliasdavid65@gmail.com>
2020-05-09 20:40:26 +0200

    Body part selection update

--- frontend/rehapp/src/app/pages/body-part-selection/body-part-selection.page.ts
+++ frontend/rehapp/src/app/pages/body-part-selection/body-part-selection.page.ts
@@ -9,7 +9,24 @@ export class BodyPartSelectionPage implements OnInit {
 
   constructor() { }
 
+  actualCircle: EventTarget;
+  position1: Array<number> = []
+
   ngOnInit() {
   }
 
+  circleClicked(element: MouseEvent){
+    // element.target['style']['fill'] = Math.floor(Math.random()*16777215).toString(16);
+    
+    if (this.actualCircle != undefined)
+      this.actualCircle['style']['fill'] = "red";
+
+    
+    this.actualCircle = element.target;
+    console.log(this.actualCircle['dataset']['majpos']);
+    
+    this.actualCircle['style']['fill'] = Math.floor(Math.random()*16777215).toString(16);
+
+    
+  }
 }
