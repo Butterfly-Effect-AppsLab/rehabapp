faf639fef93d5bdbea2c93996355c28b9aa51906
randomuserJ <goliasdavid65@gmail.com>
2020-06-25 13:28:12 +0200

    minor optimalization

--- frontend/rehapp/src/app/services/token.interceptor.ts
+++ frontend/rehapp/src/app/services/token.interceptor.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
-import { Observable, throwError, BehaviorSubject, observable, from, of } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable, throwError, BehaviorSubject } from 'rxjs';
 import { StorageService } from './storage.service';
-import { mergeMap, catchError, take, switchMap, filter, finalize } from 'rxjs/operators'
+import { catchError, take, switchMap, filter, finalize } from 'rxjs/operators'
 
-import { environment } from 'src/environments/environment'
 import { APIService } from './apiservice.service';
-
-const AUTH_URLS: Array<string> = ["users/me", "collectDiagnoses"]
-
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
@@ -17,9 +13,7 @@ export class TokenInterceptor implements HttpInterceptor {
   tokenSubject: BehaviorSubject<string> = new BehaviorSubject<string>(null);
 
   constructor(
-    private apiService: APIService,
-    private storage: StorageService
-  ) { }
+    private apiService: APIService  ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
@@ -70,81 +64,3 @@ export class TokenInterceptor implements HttpInterceptor {
     }
   }
 }
-
-  // intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-  //   const found = AUTH_URLS.find(url => url === req.url.split(environment.API_URL)[1]);
-  //   if (!found)
-  //     return next.handle(req);
-
-  //   return this.storage.getAccessToken()
-  //     .pipe(
-  //       mergeMap((token: string) => {
-  //         return next.handle(this.addAuthorizationHeader(req, token)).pipe(
-  //           catchError(
-  //             error => {
-  //               return this.handleResponseError(error, req, next);
-  //             })
-  //         )
-  //       })
-  //     )
-  // }
-
-  // addAuthorizationHeader(request: HttpRequest<any>, token: string): HttpRequest<any> {
-  //   if (token) {
-  //     const clonedRequest = request.clone({
-  //       setHeaders: {
-  //         Authorization: `Bearer ${token}`
-  //       }
-  //     })
-  //     return clonedRequest;
-  //   }
-  //   else {
-  //     console.log("Token is not available. Please check the local storage.");
-  //     return request;
-  //   }
-  // }
-
-  // handleResponseError(error, request?, next?): Observable<HttpEvent<any>> {
-  //   if (error.status === 401)
-  //     this.refreshAccessToken(error, request, next)
-
-  //   return throwError(error);
-  // }
-
-  // // // NEPOUZIVANA
-  // // handleRequest(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-  // //   return next.handle(req)
-  // //     .pipe(
-  // //       catchError((error) => {
-  // //         if (error.status !== 401)
-  // //           return throwError(error);
-
-  // //         this.refreshAccessToken()
-
-  // //         return throwError(error);
-  // //       })
-  // //     )
-  // // }
-
-  // refreshAccessToken(error, request? : HttpRequest<any>, next?){
-  //   this.storage.getItem('refresh_token').then(
-  //     (token) => {
-  //       if (token)
-  //         return this.api.refresh({ "refresh_token": token }).pipe(switchMap((response)=>{
-  //           console.log("Refresh response: ", response);
-  //               this.storage.setItem('access_token', response.body['access_token'])
-  //               return next.handle(this.addAuthorizationHeader(request, response.body['access_token']));
-  //         })).subscribe();
-
-  //       //   .subscribe(
-  //       //   (resp) => {
-  //       //     console.log("Refresh response: ", resp);
-  //       //     this.storage.setItem('access_token', resp.body['access_token'])
-  //       //     console.log(next.handle(request));
-  //       //     return next.handle(request);
-  //       //   }
-  //       // )
-  //     }
-  //   )
-  // }
-// }
