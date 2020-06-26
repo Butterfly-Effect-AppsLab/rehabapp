7a57d7e23893607d22c1d97e8e4d1003c7f1bab0
randomuserJ <goliasdavid65@gmail.com>
2020-06-27 00:38:33 +0200

    dashboard homepage rough sketch

--- frontend/rehapp/src/app/pages/dashboard/homepage/homepage.page.ts
+++ frontend/rehapp/src/app/pages/dashboard/homepage/homepage.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/services/models/User';
 import { AccountService } from 'src/app/services/account-service.service';
+import { StateService } from 'src/app/services/state-service.service';
 
 @Component({
   selector: 'app-homepage',
@@ -10,9 +11,14 @@ import { AccountService } from 'src/app/services/account-service.service';
 })
 export class HomepagePage implements OnInit {
 
+  @ViewChild('fader_top', {static: true}) topFader: ElementRef;
+  @ViewChild('fader_bot', {static: true}) botFader: ElementRef;
+  
   user: User = new User("");
+  diagnoses = ["Prva diagnoza","Druha diagnoze","Tretia diagnoza","Prva diagnoza","Druha diagnoze","Tretia diagnoza","Prva diagnoza","Druha diagnoze","Tretia diagnoza"]
+  selectedIndex = 0;
 
-  constructor(private accountService: AccountService, private router: Router) {
+  constructor(private accountService: AccountService, private router: Router, private stateService: StateService) {
   }
 
   ngOnInit() {
@@ -24,8 +30,17 @@ export class HomepagePage implements OnInit {
       this.user.username = this.accountService.userLoggedIn['name'];    
   }
 
-  logout() {
-    this.accountService.logout();
+  begin() {
+    alert(`Program ${this.diagnoses[this.selectedIndex]} 'ešte nie je pripravený :)`);
+  }
+
+  removeFader(event) {
+    this.stateService.removeFader(event, this.topFader, this.botFader)
+  }
+
+  programSelected(i) {
+    console.log(i);
+     this.selectedIndex = i;
   }
 }
 
